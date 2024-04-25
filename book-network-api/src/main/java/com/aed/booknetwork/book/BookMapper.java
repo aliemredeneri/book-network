398ed08dc9233fb93dcd4871ee4718fb5cdb7636
aliemredeneri <ali.emre.dener@gmail.com>
2024-04-25 14:29:24 +0300

    Reading book cover file

--- book-network-api/src/main/java/com/aed/booknetwork/book/BookMapper.java
+++ book-network-api/src/main/java/com/aed/booknetwork/book/BookMapper.java
@@ -1,5 +1,6 @@
 package com.aed.booknetwork.book;
 
+import com.aed.booknetwork.file.FileUtils;
 import com.aed.booknetwork.history.BookTransactionHistory;
 import org.springframework.stereotype.Service;
 
@@ -28,7 +29,7 @@ public class BookMapper {
                 .archived(book.isArchived())
                 .shareable(book.isShareable())
                 .owner(book.getOwner().getFullName())
-                //.cover(FileUtils.readFileFromLocation(book.getBookCover()))
+                .cover(FileUtils.readFileFromLocation(book.getBookCover()))
                 .build();
     }
 
